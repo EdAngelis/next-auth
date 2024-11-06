b9bc4bb0937bced9e87b53a557e3bfd4edd6ccdc
Ed Angelis <ed4ngelis@gmail.com>
2024-11-06 19:40:00 -0300

    get session

--- app/signin/signin.tsx
+++ app/signin/signin.tsx
@@ -1,15 +1,33 @@
+import { signIn, auth } from "@/auth"
 
-import { signIn } from "@/auth"
- 
-export default function SignIn() {
+interface User {
+  image: string | null;
+}
+
+interface Session {
+  user: User | null;
+}
+
+export default async function SignIn() {
+  const session = await auth();
+
+  if (!session?.user) return null;
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await signIn("google");
+  };
+
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
+    <form onSubmit={handleSubmit}>
+      <div>
+        {session.user.image ? (
+          <img src={session.user.image} alt="User Avatar" />
+        ) : (
+          <span>No Avatar</span>
+        )}
+      </div>
       <button type="submit">Signin with Google</button>
     </form>
-  )
-} 
+  );
+}
